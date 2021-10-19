3c7d75119d1cf38bd310ccc213519ae8a713a720
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-10-19 17:28:15 +0700

    Adding Custom Fonts

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
-import * as React from "react";
-import Home from "./screens/home";
+import React, {useState} from 'react';
+import Home from './screens/Home';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
 
 export default function App() {
-  return (
-    <Home />
-  )
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+
+  if(fontsLoaded){
+    return (
+      <Home />
+    )
+  } else {
+    return (
+      <AppLoading
+      startAsync={getFonts}
+      onFinish={()=> setFontsLoaded(true)} 
+      onError={() => console.log('error')}
+      />
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from "react-native";
 
 export default function ReviewDetails() {
     return (
-        <View style={styles.component}>
+        <View style={styles.container}>
             <Text>Review Details Screen</Text>
         </View>
     )
