0f948e78f9ed81b2589d8c23ba85045352b58f4e
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-10-21 10:58:24 +0700

    Navigation Options

--- routes/HomeStack.js
+++ routes/HomeStack.js
@@ -5,13 +5,24 @@ import ReviewDetails from '../screens/ReviewDetails'
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'Gamezone',
+            headerStyle: { backgroundColor: '#eee' }
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerStyle: { backgroundColor: '#D5D5D5' }
+    }
+});
 
 export default createAppContainer(HomeStack);
