26d79f99d90bb7ad67d0d95de343e1bf025bfe80
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2022-03-16 15:21:18 +0700

    Custom Card Component

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { globalStyles } from "../styles/global";
+import Card from '../shared/Card';
 
 export default function ReviewDetails({ route }) {
   const item = route.params;
   return (
     <View style={globalStyles.container}>
-      <Text>{item.title}</Text>
-      <Text>{item.body}</Text>
-      <Text>{item.rating}</Text>
+      <Card>
+        <Text>{item.title}</Text>
+        <Text>{item.body}</Text>
+        <Text>{item.rating}</Text>
+      </Card>
     </View>
   );
 }
