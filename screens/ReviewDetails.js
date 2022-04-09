4d281a7f83ce491aa006b9c1fca9043dd9cf3960
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2022-04-09 10:28:00 +0700

    Using Images

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,17 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { globalStyles } from "../styles/global";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
 import Card from '../shared/Card';
 
 export default function ReviewDetails({ route }) {
+  const { rating } = route.params;
   const item = route.params;
+  
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{item.title}</Text>
         <Text>{item.body}</Text>
-        <Text>{item.rating}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone Rating :</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  }
+});
